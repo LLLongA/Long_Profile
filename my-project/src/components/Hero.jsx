1ac0b1cdb829a4c244b99f5c0eb42a8fadbbc734
Long Chen <61738642+LLLongA@users.noreply.github.com>
2024-10-01 22:48:24 +1000

    realize the part of introduction and skills

---
+++ my-project/src/components/Hero.jsx
@@ -0,0 +1,36 @@
+import { HERO_CONTENT } from "../constants";
+import profilePic from "../assets/kevinRushProfile.png";
+const Hero = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <h1
+              className="pb-16 text-6xl font-bold tracking-tight lg:mt-16
+          lg:text-8xl"
+            >
+              Long Chen
+            </h1>
+            <span
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500
+          bg-clip-text text-4xl tracking-tighter text-transparent"
+            >
+              Front End Developer
+            </span>
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+              {HERO_CONTENT}
+            </p>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="flex justify-center">
+            <img src={profilePic} alt="Long Chen" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
