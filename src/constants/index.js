13725a7c98677da48db08b721606f06b2df0af33
Long Chen <61738642+LLLongA@users.noreply.github.com>
2024-12-19 11:23:33 +0800

    updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -20,20 +20,37 @@ With this combination of skills and interests, I aim to contribute meaningfully 
 I will graduate from ANU in June 2025. Since I may not graduate yet and do not have so much industry experience, but I am definitely qualified for this job. I have done a lot of front-end projects. I provide links for them and I will deploy some of them. You can click to view them. I sincerely hope that you can consider me and hire me. You will never regret it.\n`;
 
 export const EXPERIENCES = [
+
   {
-    year: "2021 - 2022",
+    year: "2024.11 - now",
+    role: "AI Product Manage",
+    company: "YuAiWeiWu",
+    description: `Responsible for defining and developing the vision, strategy and roadmap for AI digital human products. Analyze market trends, competitors and user needs, determine product positioning and differentiation strategies, especially in the fields of text to speech (TTS) and automatic speech recognition (ASR). Work closely with customers, business departments and technical teams to collect and analyze product requirements. Work closely with the R&D team to supervise the product development process to ensure functional implementation and quality. At the same time, perform data analysis and data annotation to support the optimization and improvement of TTS and ASR models.`,
+    technologies: ["Figma", "Python", "Au", "FrontEnd Development"],
+  },
+
+  {
+    year: "2024.6 - 2024.9",
+    role: "AI Trainer",
+    company: "Scale AI",
+    description: `In this position, I am mainly responsible for optimizing and improving the performance of the large language model in simplified Chinese by providing high-quality annotated data, testing the accuracy of model generation, and making adjustments based on the Chinese context and cultural background.`,
+    technologies: ["Sql", "Azuma"],
+  },
+  {
+    year: "2021.6 - 2021.9",
     role: "Data Journalism Internship",
     company: "NetEase",
     description: `During this internship, I worked on data-driven news stories, focusing on sourcing, cleaning, and analyzing large datasets to uncover insights that informed various journalistic reports. I collaborated with data scientists and editors to produce visually compelling data visualizations using tools like Tableau and Python. Additionally, I contributed to multimedia storytelling by assisting in the production of video content through Adobe After Effects and Premiere Pro, ensuring that complex data was presented in a clear and engaging manner.`,
     technologies: ["Python", "Tableau", "Adobe After Effects", "Adobe Premiere Pro"],
   },
   {
-    year: "2020 - 2021",
+    year: "2022.6 - 2022.9",
     role: "Data Journalism Internship",
     company: "China Business Network",
     description: `In this role, I played a critical part in delivering data-focused journalism projects. I gathered and processed data using Python, and created dynamic visualizations using D3.js to enhance reader comprehension. I regularly collaborated with cross-functional teams, including journalists, data analysts, and graphic designers, to deliver timely and impactful stories. I also used Excel for data management and analysis, ensuring accuracy in the representation of key economic and business trends.`,
     technologies: ["Python", "D3.js", "Excel"],
   },
+
 ];
 
 export const PROJECTS = [
